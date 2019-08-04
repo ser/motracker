36363883a78637365eb5c58c0316455cf1e653d0
Dr Serge Victor <ser@users.noreply.github.com>
2019-08-04 06:39:34 +0000

    GeoJSON presentation on a map

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,32 @@
 var map;
 
 function initmap() {
-  map = new L.Map('gnssmap');
+  var geotrack = $.ajax({
+    url: '/gnss/json/12',
+    dataType: 'json',
+    success: console.log('Track data successfully loaded.'),
+    error: function (xhr) {
+      alert(xhr.statusText)
+    }
+  });
+  $.when(geotrack).done(function() {
+    map = new L.Map('gnssmap');
 
-  // https://opentopomap.org/
-  // var osmUrl='https://{a|b|c}.tile.opentopomap.org/{z}/{x}/{y}.png';<Paste>
+    // https://opentopomap.org/
+    // var osmUrl='https://{a|b|c}.tile.opentopomap.org/{z}/{x}/{y}.png';<Paste>
 
-  // https://manage.thunderforest.com/dashboard
-  var osmUrl='https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey=4c4eaae4021441eea631ed97ed487943';
-  var osm = new L.TileLayer(osmUrl, {minZoom: 5, maxZoom: 30});
-  map.setView([10.78713, 106.71091], 6);
-  map.addLayer(osm);
+    // https://manage.thunderforest.com/dashboard
+    var osmUrl='https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey=4c4eaae4021441eea631ed97ed487943';
+    var osm = new L.TileLayer(osmUrl, {minZoom: 1, maxZoom: 30});
+    map.addLayer(osm);
+
+    var tracklayer = L.geoJSON(geotrack.responseJSON, {
+      style: function(feature) {
+        return { color: 'blue', weight: 4 };
+      }
+    }).addTo(map);
+
+    map.fitBounds(tracklayer.getBounds());
+  });
 }
 initmap();
